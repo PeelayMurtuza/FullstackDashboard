2091c93feb74701bab3bf87def359305a9296c74
PeelayMurtuza <peelaymurtuza@gmail.com>
2024-12-23 20:02:39 +0530

    added Signin and Signup Routes

---
+++ frontend/components/Signin.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Signin() {
+  return (
+    <div className=' bg-yellow-500 '>Im from Signin</div>
+  )
+}
+
+export default Signin;
---
+++ frontend/components/Signup.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>Im from Signup</div>
+  )
+}
+
+export default Signup;
