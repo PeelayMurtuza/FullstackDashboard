d4eee30e5f9693f09e14d703a792664f0537c06f
PeelayMurtuza <peelaymurtuza@gmail.com>
2024-12-23 20:45:04 +0530

    started backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import Signup from '../components/Signup'
+import Signin from '../components/Signin'
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className='bg-red-500 pt-2 pr-14 h-screen  '>
+        <nav className='bg-yellow-400  w-28 '>
+          <ul className='flex gap-5   '>
+            <li>
+              <NavLink to="/signup" >
+                SignUP
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/signin" >
+                SignIn
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/"  />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup/>} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
   )
 }
 
